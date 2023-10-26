85c6b0ac027ddaafb7be3a2a7f52f4f939fe3749
leo <nascimentoleo899@gmail.com>
2023-10-26 20:46:32 -0300

    adiciona validator de tamanha no cpf no domain

--- src/main/java/tech/leondev/wakivote/associado/domain/Associado.java
+++ src/main/java/tech/leondev/wakivote/associado/domain/Associado.java
@@ -2,6 +2,7 @@ package tech.leondev.wakivote.associado.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Associado {
     @NotBlank
     private String nome;
     @NotBlank
+    @Size(min = 11, max = 11, message = "O CPF deve ter exatamente 11 caracteres")
     @Column(unique = true)
     private String cpf;
 

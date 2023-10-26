c476cc1aedb240c2d356246a35d1e1b0783fd418
leo <nascimentoleo899@gmail.com>
2023-10-26 20:16:40 -0300

    implementa lista associados na camada repository

--- src/main/java/tech/leondev/wakivote/associado/application/repository/AssociadoRepository.java
+++ src/main/java/tech/leondev/wakivote/associado/application/repository/AssociadoRepository.java
@@ -2,6 +2,10 @@ package tech.leondev.wakivote.associado.application.repository;
 
 import tech.leondev.wakivote.associado.domain.Associado;
 
+import java.util.List;
+
 public interface AssociadoRepository {
     Associado salva(Associado associado);
+
+    List<Associado> lista();
 }
--- src/main/java/tech/leondev/wakivote/associado/infra/AssociadoInfraRepository.java
+++ src/main/java/tech/leondev/wakivote/associado/infra/AssociadoInfraRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import tech.leondev.wakivote.associado.application.repository.AssociadoRepository;
 import tech.leondev.wakivote.associado.domain.Associado;
 
+import java.util.List;
+
 @Log4j2
 @RequiredArgsConstructor
 @Repository
@@ -18,5 +20,13 @@ public class AssociadoInfraRepository implements AssociadoRepository {
         Associado associadoSaved = associadoSpringDataJPARepository.save(associado);
         log.info("[end] AssociadoInfraRepository - salva");
         return associadoSaved;
+    }
+
+    @Override
+    public List<Associado> lista() {
+        log.info("[start] AssociadoInfraRepository - lista");
+        List<Associado> associados = associadoSpringDataJPARepository.findAll();
+        log.info("[end] AssociadoInfraRepository - lista");
+        return associados;
     }
 }

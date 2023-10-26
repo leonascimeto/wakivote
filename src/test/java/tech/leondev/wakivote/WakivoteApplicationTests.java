8c4c0dd1c90bef2218c6f0eac78fa3097552b4d6
leo <nascimentoleo899@gmail.com>
2023-10-26 19:26:22 -0300

    configurando database na aplicação

---
+++ src/test/java/tech/leondev/wakivote/WakivoteApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leondev.wakivote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakivoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

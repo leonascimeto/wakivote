82cb18153a7bcf1a8122653351488e503e4fa926
leo <nascimentoleo899@gmail.com>
2023-10-27 20:58:21 -0300

    implementa busca sessão de votação por ID na camada repository

--- src/main/java/tech/leondev/wakivote/sessao_votacao/infra/SessaoVotacaoInfraRepository.java
+++ src/main/java/tech/leondev/wakivote/sessao_votacao/infra/SessaoVotacaoInfraRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import tech.leondev.wakivote.sessao_votacao.application.repository.SessaoVotacaoRepository;
 import tech.leondev.wakivote.sessao_votacao.domain.SessaoVotacao;
 
+import java.util.UUID;
+
 @Log4j2
 @RequiredArgsConstructor
 @Repository
@@ -17,6 +19,15 @@ public class SessaoVotacaoInfraRepository implements SessaoVotacaoRepository {
         log.info("[start] SessaoVotacaoInfraRepository - salva");
         SessaoVotacao sessaoVotacao = sessaoVtacaoSpringDataJPARepository.save(sessaoVotacaoSalva);
         log.info("[end] SessaoVotacaoInfraRepository - salva");
+        return sessaoVotacao;
+    }
+
+    @Override
+    public SessaoVotacao buscaPorId(UUID idSessaoVotacao) {
+        log.info("[start] SessaoVotacaoInfraRepository - buscaPorId");
+        SessaoVotacao sessaoVotacao = sessaoVtacaoSpringDataJPARepository.findById(idSessaoVotacao)
+                        .orElseThrow(() -> new RuntimeException("Sessão de votação não encontrada"));
+        log.info("[end] SessaoVotacaoInfraRepository - buscaPorId");
         return sessaoVotacao;
     }
 }

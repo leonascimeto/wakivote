17ee9c0024b24cd16fd1228d2dda0a6aef0c20f7
leo <nascimentoleo899@gmail.com>
2023-10-26 22:04:30 -0300

    implementa buscar pauta por ID na camada service

--- src/main/java/tech/leondev/wakivote/pauta/application/service/PautaApplicationService.java
+++ src/main/java/tech/leondev/wakivote/pauta/application/service/PautaApplicationService.java
@@ -8,6 +8,7 @@ import tech.leondev.wakivote.pauta.application.repository.PautaRepository;
 import tech.leondev.wakivote.pauta.domain.Pauta;
 
 import java.util.List;
+import java.util.UUID;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -29,5 +30,13 @@ public class PautaApplicationService implements PautaService{
         List<Pauta> pautas = pautaRepository.lista();
         log.info("[end] PautaApplicationService - lista");
         return PautaResponseDTO.convertePautasParaListaDTO(pautas);
+    }
+
+    @Override
+    public PautaResponseDTO buscaPorId(UUID idPauta) {
+        log.info("[start] PautaApplicationService - buscaPorId");
+        Pauta pauta = pautaRepository.buscaPorId(idPauta);
+        log.info("[end] PautaApplicationService - buscaPorId");
+        return new PautaResponseDTO(pauta);
     }
 }
--- src/main/java/tech/leondev/wakivote/pauta/application/service/PautaService.java
+++ src/main/java/tech/leondev/wakivote/pauta/application/service/PautaService.java
@@ -4,9 +4,12 @@ import tech.leondev.wakivote.pauta.application.api.PautaResponseDTO;
 import tech.leondev.wakivote.pauta.domain.Pauta;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PautaService {
     PautaResponseDTO salva(Pauta pauta);
 
     List<PautaResponseDTO> lista();
+
+    PautaResponseDTO buscaPorId(UUID idPauta);
 }

d65ef3970035e49192da76929114c2961c0849b6
leo <nascimentoleo899@gmail.com>
2023-10-27 22:55:03 -0300

    implementa executa voto na camada service

---
+++ src/main/java/tech/leondev/wakivote/voto/application/service/VotoApllicationService.java
@@ -0,0 +1,34 @@
+package tech.leondev.wakivote.voto.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import tech.leondev.wakivote.associado.application.repository.AssociadoRepository;
+import tech.leondev.wakivote.associado.domain.Associado;
+import tech.leondev.wakivote.sessao_votacao.application.repository.SessaoVotacaoRepository;
+import tech.leondev.wakivote.sessao_votacao.domain.SessaoVotacao;
+import tech.leondev.wakivote.voto.application.api.VotoRequestDTO;
+import tech.leondev.wakivote.voto.application.api.VotoResponseDTO;
+import tech.leondev.wakivote.voto.application.repository.VotoRepository;
+import tech.leondev.wakivote.voto.domain.Voto;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class VotoApllicationService implements VotoService{
+    private final VotoRepository votoRepository;
+    private final AssociadoRepository associadoRepository;
+    private final SessaoVotacaoRepository sessaoVotacaoRepository;
+
+    @Override
+    public VotoResponseDTO salva(VotoRequestDTO votoRequestDTO) {
+        log.info("[start] VotoApllicationService - salva");
+        Associado associado = associadoRepository.buscaPorCpf(votoRequestDTO.getCpf());
+        SessaoVotacao sessaoVotacao = sessaoVotacaoRepository.buscaPorId(votoRequestDTO.getIdSessaoVotacao());
+        sessaoVotacao.validaHorarioSessaoParaVotar();
+        Voto voto = new Voto(associado, sessaoVotacao, votoRequestDTO.getVoto());
+        Voto votoSalvo = votoRepository.salva(voto);
+        log.info("[end] VotoApllicationService - salva");
+        return new VotoResponseDTO(votoSalvo);
+    }
+}
